cb248cd6e9510ad6ef531124dee12e101040d0e8
igolubevJ <igolubevapp@gmail.com>
2025-03-19 23:09:36 +0300

    add resolve winner players feature

--- src/PokerGame.ts
+++ src/PokerGame.ts
@@ -2,6 +2,12 @@ import { ShuffleArray, WaitForSecondsAsync } from './Utils';
 import PokerPlayer from './PokerPlayer';
 import { Hand } from 'pokersolver';
 
+type PlayerWinnerInfo = {
+  player: PokerPlayer,
+  description: string,
+};
+
+
 export default class PokerGame {
   constructor() {
   }
@@ -35,12 +41,14 @@ export default class PokerGame {
 
     players.forEach(player => player.Hand = this.TakeCardsFromDeck(deck, 2))
   
+    const tableCards: string[] = [];
+
     const streets: (() => Promise<PokerPlayer>)[] = [
       () => this.PreflopAsync(),
       () => this.FlopAsync(),
       () => this.TurnAsync(),
       () => this.RiverAsync(),
-      () => this.ShowdownAsync(),
+      () => this.ShowdownAsync(players, tableCards),
     ];
 
     for (const street of streets) {
@@ -48,7 +56,7 @@ export default class PokerGame {
       await WaitForSecondsAsync(1);
       if (streetWinner != null) {
         // todo: money tranaction to winner
-        break; 
+        break;
       }
     }
   }
@@ -94,6 +102,48 @@ export default class PokerGame {
     return null;
   }
 
+  ResolveWinnerPlayers(
+    players: PokerPlayer[], 
+    tableCards: string[]
+  ) : PlayerWinnerInfo[] {
+    const hands: any[] = [];
+    const handsPlayersMap = new Map<string, any>();
+
+    players.forEach((player) => {
+      const playerFullHand = [...player.Hand, ...tableCards];
+      const slovedHand = Hand.solve(playerFullHand);
+      console.log(`Player '${player.Name}' solved hand: '${slovedHand.descr}'`);
+      hands.push(slovedHand);
+      handsPlayersMap.set(player.Name, slovedHand);
+    });
+
+    const winnerHands = Hand.winners(hands);
+
+    if (winnerHands.length === 1) {
+      const winnerHand = winnerHands[0];
+      let playerWinner: PokerPlayer = null;
+
+      for (const [k, v] of handsPlayersMap) {
+        if (v === winnerHand) {
+          playerWinner = players.find((player) => player.Name === k)
+          break;
+        }
+      }
+
+      if (!playerWinner) {
+        throw new Error('DetectWinnerHand() cannot find resolve winner!');
+      }
+
+      return [{
+        player: playerWinner,
+        description: winnerHand.descr,
+      }];
+    } else {
+      // standoff
+      return players.map((player) => ({ player, description: "STANDOFF" }))
+    }
+  }
+
   // Game state ----------------------------------------------------
   
   async PreflopAsync(): Promise<PokerPlayer> {
@@ -116,8 +166,16 @@ export default class PokerGame {
     return null;
   }
 
-  async ShowdownAsync(): Promise<PokerPlayer> {
+  async ShowdownAsync(players: PokerPlayer[], tableCards: string[]): Promise<PokerPlayer> {
     this.StepLog('SHOWDOWN');
+
+    const winners = this.ResolveWinnerPlayers(players, tableCards);
+
+    this.StepLog(`Winners`);
+    winners.forEach((winner) => {
+      console.log(`${winner.player.Name} => ${winner.description}`)
+    });
+
     return null;
   }
 
