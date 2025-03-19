155b8ee7d6efeb3977000f75ad9a720415af0746
igolubevJ <igolubevapp@gmail.com>
2025-03-19 21:00:18 +0300

    create player hand

--- src/PockerPlayer.ts
+++ src/PockerPlayer.ts
@@ -1,9 +1,18 @@
-
-
+import { BehaviorSubject } from 'rxjs';
 
 export default class PockerPlayer {
+  private _hand: BehaviorSubject<string[]>;
+
   constructor() {
+    this._hand = new BehaviorSubject<string[]>([]);
   }
 
+  public get Hand() {
+    return this._hand.value;
+  }
+
+  public set Hand(value: string[]) {
+    this._hand.next(value);
+  }
 }
 
